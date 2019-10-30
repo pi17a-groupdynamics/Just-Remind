865aa3096d34d83741cde3c5adaefd76a801d3d1
P-Vorotilenko <47428389+P-Vorotilenko@users.noreply.github.com>
2019-10-30 18:27:07 +0300

    Создал класс ShowNotificationChecker

---
+++ Just Remind/ShowNotificationChecker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Just_Remind
+{
+    class ShowNotificationChecker
+    {
+        // Флаг, указывающий, должен ли метод Work
+        // остановить работу
+        private volatile bool stopped;
+
+        // Объект главной формы
+        private Form1 mainForm;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public ShowNotificationChecker(Form1 mainForm)
+        {
+            this.mainForm = mainForm;
+        }
+
+        private void Work()
+        {
+            while (!stopped)
+            {
+                // дописать выполнение работы
+
+                Thread.Sleep(500);
+            }
+        }
+
+        /// <summary>
+        /// Команда начать работу
+        /// </summary>
+        public async void StartAsync()
+        {
+            stopped = false;
+            await Task.Run(() => Work());
+        }
+
+        /// <summary>
+        /// Команда остановить работу
+        /// </summary>
+        public void Stop()
+        {
+            stopped = true;
+        }
+    }
+}
